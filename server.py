0abf40f9a7fa55d9b262fa16f761d39d789f95f9
Bee <beegeiger@gmail.com>
2018-08-05 15:07:57 -0700

    Can now make AlertSet active or inactive using toggle button on main SafeWalk age

--- server.py
+++ server.py
@@ -876,10 +876,17 @@ def add_sched_alert(alert_set_id):
 
 @app.route("/activate/<alert_set_id>")
 def activate_alertset(alert_set_id):
-    ((db.session.query(AlertSet).filter_by(alert_set_id=alert_set_id)).update(
-    {'name':name, 'email':email, 'phone':phone, 'c_type':c_type, 'c_message':message}))
+    (db.session.query(AlertSet).filter_by(alert_set_id=alert_set_id)).update(
+    {'active': True})
     db.session.commit()
     return "Alert Set Activated"
+
+@app.route("/deactivate/<alert_set_id>")
+def deactivate_alertset(alert_set_id):
+    (db.session.query(AlertSet).filter_by(alert_set_id=alert_set_id)).update(
+    {'active': False})
+    db.session.commit()
+    return "Alert Set Deactivated"
 
 # @app.route("/add_schedset", methods=["POST"])
 # def add_sched_alertset():
