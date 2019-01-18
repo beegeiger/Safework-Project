19cd2f5458565d472ee0e2ee2d3a7674d1b146c4
Bee <beegeiger@gmail.com>
2019-01-17 18:01:43 -0800

    updated setup

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='flaskr',
-    version='1.0.0',
+    name='safeworkproject',
+    version='2.0',
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
