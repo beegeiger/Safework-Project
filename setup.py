1f186bb1b2a07f084b94850ab05917f917e960a8
Bee <beegeiger@gmail.com>
2019-01-20 15:45:14 -0800

    updated setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import find_packages, setup
-
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+    
 setup(
     name='safeworkproject',
     version='2.0',
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
-    install_requires=[
-        'flask',
-    ],
+    install_requires=requirements,
 )
