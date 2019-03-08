79d5960c099247b19f5420826a38b698633ffc84
beegeiger <beegeiger@gmail.com>
2019-03-07 17:38:21 -0800

    Fixed small issues

--- server.py
+++ server.py
@@ -38,7 +38,7 @@ app.jinja_env.undefined = StrictUndefined
 
 level = logging.DEBUG
 format = '%(asctime)s %(levelname)s %(message)s'
-handlers = [logging.FileHandler('log2.log'), logging.StreamHandler()]
+handlers = [logging.FileHandler('log3.log'), logging.StreamHandler()]
 logging.basicConfig(level = level, format = format, handlers = handlers)
 
 
